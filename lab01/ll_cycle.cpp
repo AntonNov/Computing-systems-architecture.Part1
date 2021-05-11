d049ebc631e7e0b33d655157b09e254988e1a963
Anton Novikov <95350110@study.bsuir.by>
2021-05-11 11:31:56 +0300

    wrote function

--- lab01/ll_cycle.cpp
+++ lab01/ll_cycle.cpp
@@ -1,17 +1,16 @@
 #include "ll_cycle.h"
+#include <stddef.h>
+
 
 bool ll_has_cycle(node *head) {
-	node *turtle =  head;
-	node *hare = head->next;
-	try {
-		while(turtle != hare){
-			hare->next->next;
-			turtle->next;
-		}
-		return true;
+	node *turtle = head;
+	node *hare = head;
+	while (hare != NULL && hare->next != NULL) {
+		turtle = turtle->next;
+		hare = hare->next->next;
+		if (turtle == hare)
+			return true;
+
 	}
-	catch{
-		return false;
-	}  
-	return true;
+	return false;
 }
